fa648070e4db4addace6c64ba67411dd062f7364
lewelyn7 <karol.hamielec@gmail.com>
2020-10-14 00:07:54 +0200

    mute dziala; prosty notification layer tez;

--- master-computer/master-computer.ino
+++ master-computer/master-computer.ino
@@ -4,31 +4,44 @@
 
 Timer<10, millis> timer;
 
-#define NUM_LEDS 120
+#define NUM_LEDS 90
 
 #define DATA_PIN 5
 #define CLOCK_PIN 13
 CRGB leds[NUM_LEDS];
-
+void global_set_hsv(uint8_t i, uint8_t h, uint8_t s, uint8_t v){
+  if(i < 59){
+    leds[i].setHSV((h+6)%255,s,v);
+  }else{
+    leds[i].setHSV(h,s,v);
+  }
+}
 enum class MenuStates{
-  Starting, Started
+  Starting, Started,
 };
-
+enum class SerialStates{
+  Ready, ColorReadingHSV
+};
 class Menu
 {
   public:
-  MenuStates state;
-  Menu(void){
+  MenuStates state = MenuStates::Starting;
+  SerialStates serial_state = SerialStates::Ready;
+  Menu(){
     state = MenuStates::Starting;
+    serial_state = SerialStates::Ready;
   }
-  void set(MenuStates mstate){
+  void setState(MenuStates mstate){
     state = mstate;
   }
-  void get(){
+  void getState(){
     return state;
   }
+  void setSerial(SerialStates serial){
+    serial_state = serial;
+  }
 };
-Menu MENU();
+Menu MENU;
 
 class Animation
 {
@@ -55,14 +68,14 @@ class StaticAnimation: public Animation
     : Animation(frames)
     {
       i = 0;
-      h = 60;
-      s = 200;
+      h = 100;
+      s = 100;
       v = 100;
   }  
   void tick(){
 
-    leds[(NUM_LEDS/2+i)%NUM_LEDS].setHSV(h,s,v);
-    leds[NUM_LEDS/2-(i%(NUM_LEDS/2+1))].setHSV(h,s,v);
+    global_set_hsv((NUM_LEDS/2+i)%NUM_LEDS, h,s,v);
+    global_set_hsv(NUM_LEDS/2-(i%(NUM_LEDS/2+1)), h,s,v);
     if(i == NUM_LEDS/2){
       i = 0;
     }
@@ -73,7 +86,49 @@ class StaticAnimation: public Animation
     s = s1;
     v = v1;
   }
+  void mute(){
+    global_set_hsv(40, 10, 200, 200);
+    global_set_hsv(41, 10, 200, 200);
+    global_set_hsv(42, 10, 200, 200);
+  }
+  void unmute(){
+    global_set_hsv(40, h,s,v);
+    global_set_hsv(41, h,s,v);
+    global_set_hsv(42, h,s,v);
+
+  }
+};
+class NotificationLayer: public Animation
+{
+  public:
+  uint16_t i;
+
+  bool muted = true;
+  NotificationLayer(uint8_t frames)
+    : Animation(frames)
+    {
+      i = 0;
+
+  }  
+  void tick(){
+
+    if(muted){
   
+    }else{
+      for(uint8_t j = 38; j < 43; j++){
+        global_set_hsv(j, 10, 200, 200);
+        
+      }
+  
+    }
+    i++;
+  }
+  void mute(){
+    muted = true;
+  }
+  void unmute(){
+    muted = false;
+  }
 };
 class StartAnimation: public Animation
 {
@@ -99,8 +154,8 @@ class StartAnimation: public Animation
   }
   void tick(){
    if(stage == LoadingStrip){
-    leds[(NUM_LEDS/2+i)%NUM_LEDS].setHSV(h,s,v);
-    leds[NUM_LEDS/2-(i%NUM_LEDS/2)].setHSV(h,s,v);
+    global_set_hsv((NUM_LEDS/2+i)%NUM_LEDS,h,s,v);
+    global_set_hsv(NUM_LEDS/2-(i%NUM_LEDS/2),h,s,v);
        if(i == NUM_LEDS/2){
      stage = BlinkDown;
      i = 0;
@@ -108,11 +163,11 @@ class StartAnimation: public Animation
    }
    else if(stage == BlinkDown || stage == BlinkUp){
      if(stage == BlinkDown){
-       for(uint8_t j = 0; j < NUM_LEDS; j++) leds[j].setHSV(h,s,v-(i%v));
+       for(uint8_t j = 0; j < NUM_LEDS; j++) global_set_hsv(j,h,s,v-(i%v));
      }else{
-       for(uint8_t j = 0; j < NUM_LEDS; j++) leds[j].setHSV(h,s,0 + (i%v));
+       for(uint8_t j = 0; j < NUM_LEDS; j++) global_set_hsv(j,h,s,0 + (i%v));
      }
-     if(blink_counter > 4){
+     if(blink_counter > 2){
       done_status = true;
      }
      if(stage == BlinkDown && v - 1 == (i%v)){
@@ -136,11 +191,13 @@ class AnimationsManager
 {
   private:
     Animation *curr;
+    Animation *notification;
   public:
   Queue *qu;
-  AnimationsManager(Animation *anim){
+  AnimationsManager(Animation *anim, Animation *noti){
     qu = new Queue(sizeof(curr), 10, FIFO);
     curr = anim;
+    notification = noti;
   }
   void push(Animation *anim){
     qu->push(&anim);
@@ -155,6 +212,7 @@ class AnimationsManager
       Serial.println("next");
       next();
     }
+    notification->tick();
     FastLED.show();
   }
   void next(){
@@ -168,21 +226,48 @@ uint8_t colorArray[3];
 Animation *anim;
 AnimationsManager *animManager;
 StaticAnimation *static_anim;
+NotificationLayer *notify_layer;
+//class serial buffer jest potrzebne
+char serial_buffer[20];
+uint8_t sbuff_next_idx;
+bool serial_msg_ready = false;
 bool serial_task(void){
-    if(Serial.available() >= 4){
-      Serial.println("czytam");
-      colorArray[0] = Serial.parseInt();
-      colorArray[1] = Serial.parseInt();
-      colorArray[2] = Serial.parseInt();
-
-      Serial.read();
-      Serial.print(colorArray[0]);
-      Serial.print(colorArray[1]);
-      Serial.print(colorArray[2]);
-      Serial.println("przeczytalem");
-      Serial.print(Serial.available());
-      static_anim->setHSV(colorArray[0], colorArray[1], colorArray[2]);
-
+    while(Serial.available() > 0){
+      serial_buffer[sbuff_next_idx] = Serial.read();
+      if(serial_buffer[sbuff_next_idx] == '\n'){
+        serial_msg_ready = true;
+      }
+      sbuff_next_idx++;
+      if(sbuff_next_idx == 20){
+        sbuff_next_idx = 0;
+        sbuff_next_idx = true;
+      }
+      
+    }
+    if(serial_msg_ready){
+      Serial.println("Serial");
+      if(MENU.serial_state == SerialStates::Ready){
+        if(serial_buffer[0] == 'c'){
+          serial_buffer[4] = '\0';
+          serial_buffer[8] = '\0';
+          serial_buffer[12] = '\0';
+          colorArray[0] = atoi(&serial_buffer[1]);
+          colorArray[1] = atoi(&serial_buffer[5]);
+          colorArray[2] = atoi(&serial_buffer[9]);
+          Serial.print(colorArray[0]);
+          Serial.print(colorArray[1]);
+          Serial.print(colorArray[2]);
+          
+          static_anim->setHSV(colorArray[0], colorArray[1], colorArray[2]);        
+          
+        }else if(serial_buffer[0] == 'm'){
+         notify_layer->mute();
+        }else if(serial_buffer[0] == 'u'){
+          notify_layer->unmute();
+        }
+      }
+      serial_msg_ready = false;
+      sbuff_next_idx = 0;
 }
   return true;
 
@@ -193,9 +278,12 @@ bool animation_task(void){
   return true;
 }
 void setup() {
+    sbuff_next_idx = 0;
     static_anim =  new StaticAnimation(60);
     anim = new StartAnimation(60);
-    animManager = new AnimationsManager(anim);
+    notify_layer = new NotificationLayer(60);
+    animManager = new AnimationsManager(anim, notify_layer);
+
     animManager->push(static_anim);
     Serial.begin(9600);
     // sanity check delay - allows reprogramming if accidently blowing power w/leds
